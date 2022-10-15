4c8da8b39df7eaff734c867a0bcc47ba0c4ddd22
maxguy2001 <max.guy2001@gmmail.com>
2022-10-15 09:04:59 +0100

    fixed bool_df, it  is now accurate. script now prints desired quality metrics

--- quality_check.R
+++ quality_check.R
@@ -7,10 +7,44 @@ df <- easySHARE_rel8_0_0
 #select numeric columns
 bool_df <- df %>% select_if(is.numeric)
 
-bool_df[bool_df < 0] <-0
-bool_df[bool_df >= 0] <-1
+functional_approach <- function(x){
+  return(floor(sqrt(sign(x)+1)))
+}
+
+bool_df <- map_df(bool_df, .f=functional_approach)
+
 
 bool_df$wave <- df$wave
 bool_df$country <- df$country
+
+selected_waves <- c(6,7)
+selected_countries <- c(15,16,23,25)
+
+score_combo <- function(dataframe, wavenum, countrynum){
+  df_total <- dataframe %>% 
+    filter(wave == wavenum) %>%
+    filter(country == countrynum) %>%
+    select(-c(wave, country)) %>%
+    mutate(sum = rowSums(.)) 
+  
+  total <- sum(df_total$sum)
+  
+  possible_total <- dim(df_total)[1] * dim(df_total)[2]
+  
+  quality <<- total/possible_total
+
+  return(quality)
+}
+
+
+for(i in selected_waves){
+  for(j in selected_countries){
+    quality <- score_combo(bool_df, i, j)
+    print(i)
+    print(j)
+    print(quality)
+    print(" ")
+  }
+}
 
 
