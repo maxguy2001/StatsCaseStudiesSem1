f47df10f764b05079c050505ecee524e96289141
maxguy2001 <max.guy2001@gmmail.com>
2022-10-18 22:54:52 +0100

    changed structure to use functions which modify objects in global scope

--- making_model/model_functions.R
+++ making_model/model_functions.R
@@ -37,6 +37,7 @@ removeBadColumns <-function(df){
 
 
 #replace all negative numbers with nan values
+#this function takes a while unfortunately :(
 setNanValues <- function(df){
   for(i in 1:dim(df)[1]){
     for(j in 1:dim(df)[2]){
@@ -93,28 +94,68 @@ logger <- function(string){
 
 
 findMaxCorr <- function(df_all, df_model, iternum){
+  #find maximum correlation against bmi in df_all (excluding bmi against itself)
   
   #log status
   logger(paste("Started iteration: ", iternum))
   
-  #find maximum correlation against bmi in df_all (excluding bmi against itself)
-  #move that column into df_model
-  #remove that column from df_all
-  #log information\ 
-  #log best (3?) correlated variables in dataframe
-  #return modified df_all, modified df_model
+  #get column names and make constant in current scope
+  column_names <- colnames(df_all)
+  lockBinding("column_names", environment())
+  
+  #initialize empty vector for storing correlations
+  correlations <- c()
+  
+  #populate correlations vector
+  for(i in 1:dim(df_all)[2]){
+    correlation <- cor(df_all$bmi, df_all[[i]], use="complete.obs")
+    correlations <- append(correlations, correlation)
+  }
+  
+  #sort correlations vector to get largest values
+  sorted_correlations <- sort(abs(correlations), decreasing=TRUE, na.rm=TRUE, index.return=TRUE)
+  
+  #log best (3) correlated variables in dataframe
+  best_3_cors_column_names <- column_names[sorted_correlations$ix[2:4]]
+  best_3_cors_values <- sorted_correlations$x[2:4]
+  logger(paste("Best columns: ", best_3_cors_column_names))
+  logger(paste("With absolute correlation values: ", best_3_cors_values))
+  
+  sorted_correlations$colnames <- column_names[sorted_correlations$ix]
+  
+  return(sorted_correlations)
 }
+
+
+switchColumnToModel <- function(df_all, df_model, column_to_switch){
+  #switches column from df_all to df_model
+  #note no direct return type, only modifies objects in the global scope
+  df_model[[column_to_switch]] <<- df_all[[column_to_switch]]
+  df_all <<- df_all %>%
+    select(-c(column_to_switch))
+}
+
 
 replaceNanVals <- function(df_model){
   #replaces the nan values in a column with the mean of the column
+  df_model <<- df_model %>%
+    mutate_all(~ifelse(is.na(.x), mean(.xm na.rm = TRUE), .x))
 }
 
 removeNanVals <- function(df_model){
   #removes all nan values from dataframe
+  df_model <<- df_model %>%
+    drop_na()
 }
 
 makeModel <- function(df_model, iternum, nan_method = "remove"){
   #remove all na from dataframe or replace with mean?
+  if(nan_method == "remove"){
+    removeNanVals()
+  }else{
+    replaceNanVals()
+  }
+  
   #construct linear model of bmi against rest of dataframe
   #return linear model?
 }
@@ -134,20 +175,3 @@ quantifyImprovements <- function(iternum){
   #improved from model n-1
   #add 1 to iteration number
 }
-
-idea <- function(df_all, df_model){
-  column_names <- colnames(df_all)
-  lockBinding("column_names", environment())
-  
-  correlations <- c()
-  
-  for(i in 1:dim(df_all)[2]){
-    correlation <- cor(df_all$bmi, df_all[[i]], use="complete.obs")
-    correlations <- append(correlations, correlation)
-  }
-  sorted_correlations <- sort(correlations, decreasing=TRUE, na.rm=TRUE, index.return=TRUE)
-  top_three_cor_columns <- sorted_correlations$ix[2:4]
-  #TODO: figure out a way to choose what column to add to model df.
-  return(correlations)
-}
-
